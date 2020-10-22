0a3feb3f744294d3c33f10564c05db79779e1a47
LeandroParisi <lepcbelisario@gmail.com>
2020-10-22 14:47:54 -0300

    first commit

---
+++ src/average.js
@@ -0,0 +1,34 @@
+/*
+  A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
+  Caso a função receba algum valor não númerico ou um array vazio,
+  o valor undefined deve ser retornado.
+  Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
+
+  Parâmetros:
+    - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
+  Comportamento:
+    - average([2, 2]) // Retorno: 2;
+    - average([1, 2]) // Retorno: 1;
+    - average([1, '2']) // Retorno: undefined;
+*/
+
+const average = (inputedArray) => {
+  if (inputedArray.length <= 0) return undefined;
+  for (index = 0; index < inputedArray.length; index += 1) {
+    if (typeof (inputedArray[index]) === 'string') {
+      return undefined;
+    }
+  }
+  let averageResult = 0;
+  for (index = 0; index < inputedArray.length; index += 1) {
+    averageResult += inputedArray[index];
+  }
+  const divisor = inputedArray.length;
+  const result = averageResult / divisor;
+  return Math.round(result);
+};
+console.log(average([1, 2, 3, 4]));
+console.log(average([]));
+console.log(average([1, 2, '3']));
+
+module.exports = average;
---
+++ src/numbers.js
@@ -0,0 +1,29 @@
+/*
+  A função numbers recebe um array (tamanho variável),
+  retornando true se todos os parâmetros forem do tipo 'number' e false caso contrário.
+
+  Parâmetros:
+    - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, 'a']; [].
+  Comportamento:
+    - numbers([2, 3, 4]); // Retorna: true
+    - numbers([2, 'errado', 5]); // Retorna: false
+*/
+
+const numbers = (myArray) => {
+  for (let i = 0; i < myArray.length; i += 1) {
+    if (typeof myArray[i] !== 'number') {
+      return false;
+    }
+  }
+  return true;
+};
+
+module.exports = numbers;
+
+
+// const assert = require('assert');
+// assert.equal(numbers([1, 2, 3, 4, 5]), true, 'Result should be TRUE')
+// assert.equal(numbers([1, 2, "3", 4, 5]), false, 'Result should be FALSE');
+// assert.equal(numbers([1, 'a', 3]), false, 'Result should be FALSE');
+// assert.equal(numbers([' ']), false, 'Result should be FALSE');
+
